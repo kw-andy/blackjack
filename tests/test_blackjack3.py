c249c36471d8e00f6e8c5f8b524b7fa36b84792f
unknown <andy.kwok.work@gmail.com>
2023-06-07 12:12:19 +0200

    initial commit

---
+++ tests/test_blackjack3.py
@@ -0,0 +1,16 @@
+import pytest
+
+from blackjack.common import card_score
+
+
+@pytest.mark.parametrize("cards, score", [("KK", 20), ("JKQ", 0), ("AK", 21), ("AA", 12)])
+def test_simple_case_1(cards, score):
+    assert card_score(cards) == score
+
+def test_number_of_cards1():
+    with pytest.raises(ValueError):
+        card_score("")
+
+def test_number_of_cards2():
+    with pytest.raises(ValueError):
+        card_score(1)
